15b64046715caf45b1295ac6b6085e31fe07f0dc
Soesh Cooray <it24101009@my.sliit.lk>
2025-10-20 18:53:54 +0530

    Update EMAIL_HOST_PASSWORD in production settings for secure configuration

--- personal_budget_manager/settings_production.py
+++ personal_budget_manager/settings_production.py
@@ -49,7 +49,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'budgetmaster2025@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'mrlo pfps uaog pcjo')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # CORS settings for production
