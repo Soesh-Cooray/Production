0483434d1393bf4608cadfc679480af30ff9aa3d
Soesh Cooray <it24101009@my.sliit.lk>
2025-10-20 15:11:43 +0530

    Update deployment configuration

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+echo "Python Version:"
+/usr/local/bin/python3.12 --version
+echo "Installing dependencies..."
+/usr/local/bin/python3.12 -m pip install --upgrade pip
+/usr/local/bin/python3.12 -m pip install -r requirements.txt
+echo "Collecting static files..."
+/usr/local/bin/python3.12 manage.py collectstatic --noinput --clear
