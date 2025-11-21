3c4a331a380cb831b213896fc143d7a6adf03b04
Soesh Cooray <it24101009@my.sliit.lk>
2025-11-21 12:40:21 +0530

    Add build script and requirements file

--- Backend/build_files.sh
+++ Backend/build_files.sh
@@ -1,5 +1,6 @@
 echo "Building the project..."
 python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
