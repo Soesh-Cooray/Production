bb3301240a3a0f2669109e6ebda3e193cbd762b5
Soesh Cooray <soeshcooray@gamil.com>
2025-11-29 18:33:31 +0530

    Deleted unneccasry files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Python Version:"
-/usr/local/bin/python3.12 --version
-echo "Installing dependencies..."
-/usr/local/bin/python3.12 -m pip install --upgrade pip
-/usr/local/bin/python3.12 -m pip install -r requirements.txt
-echo "Collecting static files..."
-/usr/local/bin/python3.12 manage.py collectstatic --noinput --clear
